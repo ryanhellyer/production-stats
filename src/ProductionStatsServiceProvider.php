70c03bafb2eefa8f3192fe8e9d7c892859f68746
Ryan <ryan@hellyer.kiwi>
2026-07-21 12:22:48 +0200

    feat: add deprecated backward compatibility stub for ProductionStatsServiceProvider

---
+++ src/ProductionStatsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RyanHellyer\ProductionStats;
+
+use RyanHellyer\ProductionStats\Laravel\ProductionStatsServiceProvider as NewServiceProvider;
+
+/**
+ * @deprecated Use \RyanHellyer\ProductionStats\Laravel\ProductionStatsServiceProvider instead.
+ *             This stub exists for backward compatibility during upgrades
+ *             when the cached package manifest still references the old FQCN.
+ */
+class ProductionStatsServiceProvider extends NewServiceProvider
+{
+}
